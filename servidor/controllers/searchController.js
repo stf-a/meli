0e856978dc4efc5f3819dd44a0e22bf0516ba35d
Estefania Aguilar <silestagui@gmail.com>
2018-07-18 12:06:29 -0300

    before review

--- servidor/controllers/searchController.js
+++ servidor/controllers/searchController.js
@@ -1,12 +1,48 @@
-const searchService = require('../services/searchService')
-
 let self = {}
 
-self.index = function(req, res){	
-    const query = req.params.id
+const searchService = require('../services/searchService')
 
-    searchService.getItemData(query).then(function(result){
-        return res.json({data: result})
+self.index = function(req, res){	
+    const query = req.query.search;
+
+    const search = searchService.getItemData(query).then(function(resultado){
+        
+        let author = {
+            name:'asd',
+            lastname: 'asd'
+        }
+
+        let categories = []
+
+        let items = []
+
+        for(var i =0; i<results.results.length; i++){
+          const obj = {
+              categories:categories,
+              id:resultado.results[i].id,
+              title:resultado.results[i].id,
+              price:{
+                  currency:resultado.results[i].currency_id,
+                  amount:resultado.results[i].installments.amount
+                    },
+              picture:resultado.results[i].thumbnail,
+              condition:resultado.result[i].condition,
+              free_shiping:resultado.results[i].shipping.free_shipping
+                }
+            
+                items.push(obj)      
+            }
+
+        const busquedaCuatro = {
+            author: author,
+            categories:categories,
+            items:items
+            }
+
+       return res.json(busquedaCuatro)
+        
+    }).catch(function(err){
+       console.log(err) 
     })
 }   
 
--- servidor/services/searchService.js
+++ servidor/services/searchService.js
@@ -3,20 +3,16 @@ let self = {}
 const restler = require('restler');
 
 self.getItemData = function(query){
+  console.log('entro al get item data')
+  console.log(query)
     const itemData = new Promise(function(resolve, reject){
-        restler.get('https://api.mercadolibre.com/sites/MLA/search?q=​:'+ query + '&limit=4').on('complete', function(result){
+        restler.get('https://api.mercadolibre.com/sites/MLA/search?q=​'+ query + '&limit=4').on('complete', function(result){
             resolve(result)
-            console.log(result)
         }).on('fail', function(err){
-            console.log(err)
             reject(err)
         })
     })
     return itemData
 }
-
-
-
-
 
 module.exports = self;
